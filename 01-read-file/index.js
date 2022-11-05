c04e0dd43a9486c3e223f91b216f26c273dec240
Maria Danisman <martorija@mail.ru>
2022-11-05 15:09:10 +0300

    refactor: changed index.js

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
-const stream = new fs.ReadStream( "01-read-file/text.txt", "utf8");
-stream.on('readable',function(err, data){
+const path = require('path');
+const stream = new fs.ReadStream( path.join(__dirname, "text.txt"), 'utf-8');
+stream.on('readable',(err, data) => {
     if(err) throw err;
     data = stream.read();
     if(data !=null)console.log(data);
