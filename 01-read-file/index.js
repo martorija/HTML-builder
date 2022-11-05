7791191f4a46ccac281895913ee42a38b3987891
Maria Danisman <martorija@mail.ru>
2022-11-05 14:38:06 +0300

    refactor: changed index.js

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
-fs.readFile('01-read-file/text.txt', 'utf8', (err, fileContent)=>{
+const stream = new fs.ReadStream( "01-read-file/text.txt", "utf8");
+stream.on('readable',function(err, data){
     if(err) throw err;
-    console.log(fileContent);
+    data = stream.read();
+    if(data !=null)console.log(data);
 });
